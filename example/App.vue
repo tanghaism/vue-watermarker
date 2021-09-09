a4e753c2837dad8f2d4c1ae92a90b22b5b04bd52
唐海 <280503392@qq.com>
2021-09-09 11:16:53 +0800

    docs: 修改example

--- example/App.vue
+++ example/App.vue
@@ -7,34 +7,18 @@
 
   <!-- waterMarker -->
   <VueWaterMarker
-    :visible="init"
     :content="['water marker one', '__water marker two__', '~~~~~water marker three~~~~']"
   />
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from "vue"
-import VueWaterMarker from '../src/index'
+import {defineComponent} from "vue"
+import VueWaterMarker from 'vue-watermarker'
 export default defineComponent({
   name: "App",
   components: {
     VueWaterMarker
-  },
-  setup(){
-
-    const init = ref<boolean>(true);
-
-    setTimeout(() => {
-      init.value = false;
-      setTimeout(() => {
-        init.value = true;
-      }, 2000)
-    }, 2000)
-
-    return {
-      init
-    }
-  },
+  }
 })
 </script>
 
